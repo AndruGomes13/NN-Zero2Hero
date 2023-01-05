496ddab4129fa8a0eb366cff53a7ad6e6903f901
AndruGomes13 <79923141+AndruGomes13@users.noreply.github.com>
2023-01-05 08:54:29 +0000

    Adding NN files

---
+++ afkbot.py
@@ -0,0 +1,13 @@
+import pyautogui as pag
+import random
+import time
+
+t = time.time()
+
+
+while True:
+    #x = random.randint(200, 1000)
+    #y = random.randint(200, 600)
+    #pag.moveTo(x,y, 1)
+    time.sleep(10)
+    pag.leftClick()
